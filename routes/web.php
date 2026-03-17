9b7c2b8ed8ad683fca807fc7c2582e421a9c7df9
fran-asensio <fasensio.dev@gmail.com>
2026-03-17 20:05:09 +0100

    Añadida generación de QR en backend

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use SimpleSoftwareIO\QrCode\Facades\QrCode;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/qr/{id}', function ($id) {
+    
+    $url = "https://fran-asensio.github.io/el-pozo-frontend/#/producto/" . $id;
+    return QrCode::size(300)->generate($url);
 });
